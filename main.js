4c00abbdd89b929cee0a92e68b967a4bcfa28d28
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-02-10 01:34:58 +0600

    amar javassript done

---
+++ main.js
@@ -0,0 +1,15 @@
+const products = document.getElementById('product');
+const li = document.createElement('li')
+li.innerText="amar sonar bangla ami tomay valobasi";
+products.appendChild(li)
+const college= document.getElementById('product');
+const h1 = document.createElement('h1')
+h1.innerText= 'dhaka international university,dhaka';
+college.appendChild(h1)
+const color =document.getElementById('product');
+const h1= document.body.style.backgroundColor = 'red'
+color.appendChild(h1)
+const box = document.getElementsByClassName(box-1);
+const box1=document.body.style.backgroundColor = "#AA0000";
+const alla = document.getElementsByTagNameNS('container')
+console.log(alla)
