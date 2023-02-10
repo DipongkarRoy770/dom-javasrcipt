d603808f2862f014b4fe1301e6cd07c7f194b629
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-02-10 16:17:59 +0600

    click working done

---
+++ onclick/index.js
@@ -0,0 +1,11 @@
+function handleOnclick(){
+  const write = document.getElementById('title-text')
+  write.innerText = 'amr sonar bangla ami tomay valobasi';
+}
+document.getElementById('click-me').addEventListener('click',function(){
+  const title = document.getElementById('title-text')
+  title.innerText = 'delta computer science collage ,rangpur';
+})
+// document.getElementById('click-me').addEventListener('click',function(){
+//   document.body.style.backgroundColor = 'pink';
+// })
