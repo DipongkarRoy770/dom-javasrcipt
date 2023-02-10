6c928c3876594fd4e43c701ef6a264df4e04305a
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-02-10 15:19:26 +0600

    welldone

---
+++ index.js
@@ -0,0 +1,8 @@
+const color =document.getElementById('text');
+color.style.backgroundColor = 'red';
+const itemList = document.getElementById('list');
+const li = document.createElement('li')
+li.innerText='delta computer science collage,rangpur';
+li.style.backgroundColor = 'pink';
+li.style.fontSize = '1.5em'
+itemList.appendChild(li);
